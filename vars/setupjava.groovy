c2acb0eac59184fa03b32097cf505c36113b0d2a
80Pooja <poodevagirikar@gmail.com>
2025-02-20 02:08:56 +0530

    Update setupjava.groovy

--- vars/setupjava.groovy
+++ vars/setupjava.groovy
@@ -1,7 +1,7 @@
 def call(cmd) {
-echo " installing dependancy"
+echo " installing dependancy "
   sh "sudo apt update"
 //  sh "sudo apt install -y openjdk-17-jdk"
-  echo " $(cmd) "
+  echo " ${cmd} "
   sh "sudo apt install -y ${cmd}"
 }
